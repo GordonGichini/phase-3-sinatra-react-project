52e9e5021ea377bf85d88bfe11b74550ea8d3018
GordonGichini <gordongichini1@gmail.com>
2022-09-16 04:23:43 +0300

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
-puts "🌱 Seeding spices..."
+
 
 # Seed your database here
+User.create(name: "Scott",email: "scott123@email",password: "scott321",phone: "103-956",town: "Nairobi")
+User.create(name: "Jones",email: "jones123@email",password: "jones321",phone: "221-561",town: "Nyeri")
+User.create(name: "Gordon",email: "don123@email",password: "gordon321",phone: "998-098",town: "Nakuru")
+User.create(name: "Maria",email: "mary123@email",password: "mary321",phone: "675-897",town: "Njonjo")
+User.create(name: "Ivy",email: "ivy123@email",password: "ivy321",phone: "657-987",town: "Kakamega")
 
-puts "✅ Done seeding!"
+
+Product.create(name: "Iphone13",price: "899")
+Product.create(name: "Sony Camera",price: "499")
+Product.create(name: "Samsung QLED TV",price: "1999")
+Product.create(name: "Ipad Pro",price: "567")
+Product.create(name: "Xbox",price: "699")
+Product.create(name: "Mac",price: "899")
